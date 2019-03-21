3bacb338d834806816382cdd15e8454b23177ffe
hp <panghongbox@gmail.com>
2019-03-21 14:17:03 -0400

    version 0.0.1 change readme

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup
 setup(
     name='convert2py',
-    url='https://github.com/jladan/package_demo',
+    url='https://github.com/nebulaai/orion-script-converter',
     author='Eric Pang',
     packages=['converter'],
     version='0.0.1',
     license='MIT',
-    description='An python package for converting Jupyter Notebook .ipynb files into python 3 .py files'
+    description='An python package for converting Jupyter Notebook ".ipynb" files into python 3 ".py" files'
                 ' and making a task file that can be run on Orion platform.',
     entry_points={
         'console_scripts': [
